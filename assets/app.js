f697ef2209f0a6edcc1d07792fd1675fc270a718
Milo <milo.dev.web@gmail.com>
2021-05-19 15:22:20 +0200

    premier envoi du projet art d'un temps

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+
+const $ = require("jquery");
+global.$ = global.jQuery = $;
+window.Popper = require("popper.js");
+require("bootstrap");
+
+//import './bootstrap';
+console.log("chargement du fichier app.js");
