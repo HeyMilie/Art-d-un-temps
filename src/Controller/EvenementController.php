9fd2dd2966d203fdd655f5c4d0f65a8f11911c71
heymilie <emilie.delaurent@gmail.com>
2021-05-21 06:47:14 +0200

    Correction use Repository dans EvenementController et MembreController

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Evenement;
 use App\Form\EvenementType;
-use App\Entity\EvenementRepository;
+use App\Repository\EvenementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Membre;
 use App\Form\MembreType;
-use App\Entity\MembreRepository;
+use App\Repository\MembreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
