99978fc93e17dc8991fa75f059f6afa326f40d10
Milo <milo.dev.web@gmail.com>
2021-05-21 18:09:34 +0200

    Création du formulaire d'inscription et ajout de la ligne bootstrap dans twig.yaml

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -19,6 +19,6 @@ class ProfilController extends AbstractController
     {
          return $this->render('profil/index.html.twig');
 
-        ]);
+        /* ]); */
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,8 +9,9 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    #[Route('/connexion', name: 'app_login')]
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/connexion", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -27,7 +28,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/deconnexion", name="app_logout")
      */
     public function logout()
     {
