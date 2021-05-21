bb4c4161b75053d6a321ea73aa55f9d471e20261
siaka <siaka95@gmail.com>
2021-05-21 09:43:03 +0200

    ajout du logo

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,20 @@ Encore
 //.enableIntegrityHashes(Encore.isProduction())
 
 // uncomment if you're having problems with a jQuery plugin
-.autoProvidejQuery();
+.autoProvidejQuery()
+.copyFiles({
+    from: './assets/images',
+    // optional target path, relative to the output dir
+    to: '../images/[path][name].[ext]',
+    /*
+    // if versioning is enabled, add the file hash too
+    //to: 'images/[path][name].[hash:8].[ext]',
+    // only copy files matching this pattern
+    //pattern: /\.(png|jpg|jpeg)$/
+    */
+    })
+
+    
+;
 
 module.exports = Encore.getWebpackConfig();
