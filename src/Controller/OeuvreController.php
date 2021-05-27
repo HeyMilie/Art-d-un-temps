4123a9976ccb42395f5a870433fde97c4e389a73
heymilie <emilie.delaurent@gmail.com>
2021-05-27 18:05:53 +0200

    Crud Oeuvres Liste et New

--- src/Controller/OeuvreController.php
+++ src/Controller/OeuvreController.php
@@ -10,10 +10,24 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/oeuvre')]
+
 class OeuvreController extends AbstractController
 {
-    #[Route('/', name: 'oeuvre_index', methods: ['GET'])]
+    #[Route('/oeuvres', name: 'oeuvres', methods: ['GET'])]
+    public function homeOeuvres(OeuvreRepository $oeuvreRepository): Response
+    {
+        return $this->render('oeuvre/oeuvres.html.twig', [
+            'oeuvres' => $oeuvreRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/oeuvres/oeuvre{id}', name: 'oeuvre', methods: ['GET'])] 
+    public function ficheOeuvre(OeuvreRepository $oeuvreRepository): Response
+    {
+        return $this->render('oeuvre/oeuvre.html.twig', ['oeuvres' => $oeuvreRepository->findAll(),]);
+    }
+
+    #[Route('artiste/oeuvres/', name: 'bo_oeuvres', methods: ['GET'])]
     public function index(OeuvreRepository $oeuvreRepository): Response
     {
         return $this->render('oeuvre/index.html.twig', [
@@ -21,13 +35,7 @@ class OeuvreController extends AbstractController
         ]);
     }
 
-    #[Route('/oeuvres', name: 'oeuvre_oeuvres', methods: ['GET'])] 
-    public function oeuvres(OeuvreRepository $oeuvreRepository): Response
-    {
-        return $this->render('oeuvre/oeuvres.html.twig', ['oeuvres' => $oeuvreRepository->findAll(),]);
-    }
-
-    #[Route('/new', name: 'oeuvre_new', methods: ['GET', 'POST'])]
+    #[Route('artiste/oeuvre/new', name: 'oeuvre_new', methods: ['GET', 'POST'])]
     public function new(Request $request): Response
     {
         $oeuvre = new Oeuvre();
@@ -39,7 +47,7 @@ class OeuvreController extends AbstractController
             $entityManager->persist($oeuvre);
             $entityManager->flush();
 
-            return $this->redirectToRoute('oeuvre_index');
+            return $this->redirectToRoute('bo_oeuvres');
         }
 
         return $this->render('oeuvre/new.html.twig', [
@@ -48,7 +56,7 @@ class OeuvreController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'oeuvre_show', methods: ['GET'])]
+    #[Route('artiste/oeuvre/{id}', name: 'oeuvre_show', methods: ['GET'])]
     public function show(Oeuvre $oeuvre): Response
     {
         return $this->render('oeuvre/show.html.twig', [
@@ -56,7 +64,7 @@ class OeuvreController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'oeuvre_edit', methods: ['GET', 'POST'])]
+    #[Route('artiste/oeuvre/{id}/edit', name: 'oeuvre_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Oeuvre $oeuvre): Response
     {
         $form = $this->createForm(OeuvreType::class, $oeuvre);
@@ -65,7 +73,7 @@ class OeuvreController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('oeuvre_index');
+            return $this->redirectToRoute('bo_oeuvres');
         }
 
         return $this->render('oeuvre/edit.html.twig', [
@@ -74,7 +82,7 @@ class OeuvreController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'oeuvre_delete', methods: ['POST'])]
+    #[Route('artiste/oeuvre/{id}', name: 'oeuvre_delete', methods: ['POST'])]
     public function delete(Request $request, Oeuvre $oeuvre): Response
     {
         if ($this->isCsrfTokenValid('delete' . $oeuvre->getId(), $request->request->get('_token'))) {
@@ -83,6 +91,6 @@ class OeuvreController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('oeuvre_index');
+        return $this->redirectToRoute('bo_oeuvres');
     }
 }
