1085f9fb679540da075a84f7bf5397c0baa472f3
heymilie <emilie.delaurent@gmail.com>
2021-05-31 19:25:51 +0200

    Affichage des événements

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\EvenementRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EventController extends AbstractController
 {
     #[Route('/event', name: 'event')]
-    public function index(): Response
+    public function index(EvenementRepository $evenementRepository): Response
     {
         return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
+            'evenements' => $evenementRepository->findAll(),
         ]);
     }
 }
